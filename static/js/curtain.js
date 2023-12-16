e21172004d6794ba9d9b3726737b9b686fd88bfa
Ganesan, Gowtham P <gowthamprakaash@gmail.com>
2023-12-16 23:17:45 +0530

    something

--- static/js/curtain.js
+++ static/js/curtain.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     setTimeout(function () {
-        $(".theatre").addClass("curtain-open");
+        $(".curtains").addClass("curtain-open");
 
-        // setTimeout(function () {
-        //     // $(".theatre").css("display", "none");
-        //     $(".theatre").css("z-index", "-1");
-        // }, 3000);
+        setTimeout(function () {
+            // $(".theatre").css("display", "none");
+            // $(".theatre").css("z-index", "-1");
+            $(".music-anim").css("display", "inherit");
+        }, 3000);
     }, 1000);
 })
