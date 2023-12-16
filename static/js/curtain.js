5980e80e5e84f992cde0165746fc64613d17e143
Ganesan, Gowtham P <gowthamprakaash@gmail.com>
2023-12-16 17:16:21 +0530

    Initial commit

---
+++ static/js/curtain.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".theatre").addClass("curtain-open");
+
+        // setTimeout(function () {
+        //     // $(".theatre").css("display", "none");
+        //     $(".theatre").css("z-index", "-1");
+        // }, 3000);
+    }, 1000);
+})
