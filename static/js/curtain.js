4bff60ce74a1fc28960b1a3c2ef52c8629a72656
Ganesan, Gowtham P <gowthamprakaash@gmail.com>
2023-12-17 22:36:52 +0530

    snowflakes and jingle bells

--- static/js/curtain.js
+++ static/js/curtain.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
             // $(".theatre").css("display", "none");
             // $(".theatre").css("z-index", "-1");
             $(".music-anim").css("display", "inherit");
+            $(".snowflakes").css("display", "inherit");
+            $("#current-theme-song").get(0).load();
         }, 3000);
     }, 1000);
 })
