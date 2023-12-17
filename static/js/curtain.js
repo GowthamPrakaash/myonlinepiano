7d89b773a697d7c1304b5082f5af3ebcee56d4a2
Ganesan, Gowtham P <gowthamprakaash@gmail.com>
2023-12-18 00:03:46 +0530

    some fixes

--- static/js/curtain.js
+++ static/js/curtain.js
@@ -1,13 +1,30 @@
-$(document).ready(function () {
-    setTimeout(function () {
-        $(".curtains").addClass("curtain-open");
+function openCurtain(){
+    // setTimeout(function () {
+    $(".curtain-scissor").css("visibility","hidden");
+    $(".curtain-scissor").css("opacity","0");
+    $(".curtains").addClass("curtain-open");
 
-        setTimeout(function () {
-            // $(".theatre").css("display", "none");
-            // $(".theatre").css("z-index", "-1");
-            $(".music-anim").css("display", "inherit");
-            $(".snowflakes").css("display", "inherit");
-            $("#current-theme-song").get(0).load();
-        }, 3000);
-    }, 1000);
-})
+    setTimeout(function () {
+        // $(".theatre").css("display", "none");
+        // $(".theatre").css("z-index", "-1");
+        $(".music-anim").css("display", "inherit");
+        $(".snowflakes").css("display", "inherit");
+        playAudio();
+        $(".music-control").show();
+    }, 2000);
+    // }, 1000);
+}
+
+function playAudio() {
+    $(".music-control-play").hide();
+    $(".music-control-pause").show();
+    $("#current-theme-song").get(0).play();
+    $(".music-control").attr("onclick", "pauseAudio();");
+}
+
+function pauseAudio() {
+    $(".music-control-play").show();
+    $(".music-control-pause").hide();
+    $("#current-theme-song").get(0).pause();
+    $(".music-control").attr("onclick", "playAudio();");
+}
